eb5671c984aa2feff956eaeb14879d68e83e68a9
chr1scr0sss <tiwakorn.matonboon@gmail.com>
2024-12-10 21:08:32 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,52 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+using namespace std;
+
+int main(){
+    double age, height, bounty;
+    string character;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    
+
+
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+
+    if(height < 100){
+        character = "Chopper";
+    } else if(height < 180){
+        character = "Usopp";
+    } else {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+
+        if(bounty <= 1100000000){
+            character = "Sanji";
+        } else {
+            character = "Zoro";
+        }
+    }
+    } else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000){
+            character = "Jinbe";
+        }else {
+            character = "Franky";
+        }
+    } 
+    
+    else if(age > 60) {
+        character = "Brook";
+    }
+    
+    
+    
+    
+    cout << "Your character = " << character;
+}
